3541e45c15e7a556ad66972e9ccfd759e686d556
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-09-10 20:42:06 +0545

    config defaults: owner 8841848847, SAMAR repo

--- config.py
+++ config.py
@@ -4,23 +4,24 @@ from dotenv import load_dotenv
 
 if os.path.exists("local.env"):
     load_dotenv("local.env")
+elif os.path.exists(".env"):
+    load_dotenv(".env")
 
+API_ID = int(getenv("API_ID", "34979297") or "34979297")
+API_HASH = getenv("API_HASH", "0e45dd9855674f34f91705734d7642b4") or "0e45dd9855674f34f91705734d7642b4"
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
-
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7553434931").split()))
-OWNER_ID = int(getenv("OWNER_ID","7746761207"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "8841848847").split()))
+OWNER_ID = int(getenv("OWNER_ID", "8841848847"))
 MONGO_URL = getenv("MONGO_URL")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://files.catbox.moe/lo4efn.jpg')
-ALIVE_TEXT = getenv("ALIVE_TEXT")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://files.catbox.moe/lo4efn.jpg")
+ALIVE_TEXT = getenv("ALIVE_TEXT", "SAMAR Userbot")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
-GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/Hkmusic/AJAY-USERBOT")
-BRANCH = getenv("BRANCH", "master") #don't change
- 
+GIT_TOKEN = getenv("GIT_TOKEN")
+REPO_URL = getenv("REPO_URL", "https://github.com/tsamarthakur515-spec/SAMAR_2USERBOT")
+BRANCH = getenv("BRANCH", "master")
+
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
