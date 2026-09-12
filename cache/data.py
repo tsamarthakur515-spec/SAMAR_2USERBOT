cea747a96ab1b8e78fcfb14c684a695b7a8a2306
tsamarthakur515-spec <tsamarthakur515@gmail.com>
2026-09-12 16:29:15 +0545

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -2,12 +2,12 @@
 GROUP = []
 VERIFIED_USERS = [8841848847]
 
-RAID = ["!"]
-PIRAID = ["!"]
-PBIRAID = ["!"]
-HIRAID = ["!"]
-PORN = ["!"]
-PORM = ["!"]  # typo alias used in rr.py / replyraid.py
+RAID = ["𝐓𝐄𝐑𝐈 𝐌𝐀𝐀 𝐊𝐀 𝐁𝐇𝐎𝐒𝐃𝐀 𝐑𝐄𝐘 𝐌𝐀𝐃𝐄𝐑𝐂𝐇𝐎𝐃 😛🤣"]
+PIRAID = ["𝐔𝐓𝐇 𝐓𝐄𝐑𝐈 𝐌𝐀𝐈 𝐊𝐀 𝐁𝐔𝐑 𝐌𝐀𝐈𝐍 𝐂𝐇𝐀𝐏𝐏𝐀𝐋 𝐌𝐀𝐑𝐔 🥹🌚"]
+PBIRAID = ["𝐊𝐀𝐈𝐒𝐀 𝐌𝐀𝐑𝐀 𝐑𝐀𝐘 𝐌𝐀𝐃𝐄𝐑𝐂𝐇𝐎𝐃 𝐊𝐀 𝐁𝐀𝐂𝐇𝐀 🙂‍↕️😝"]
+HIRAID = ["𝐅𝐈𝐑 𝐒𝐀 𝐓𝐄𝐑𝐈 𝐌𝐀𝐀 𝐊𝐀 𝐆𝐎𝐃𝐇 𝐁𝐇𝐀𝐑𝐀𝐈 𝐇𝐎 𝐉𝐀𝐘𝐀𝐆𝐀 ☺️🫪"]
+PORN = ["𝐓𝐄𝐑𝐈 𝐁𝐇𝐄𝐍 𝐊𝐈 𝐆𝐔𝐋𝐀𝐁𝐈 𝐂𝐇𝐔𝐓 𝐌𝐀𝐈𝐍 𝐏𝐀𝐍𝐈 𝐃𝐀𝐋𝐔 😂😂"]
+PORM = ["𝐓𝐄𝐑𝐈 𝐌𝐀𝐈 𝐊𝐀 𝐁𝐔𝐑 𝐌𝐀𝐈𝐍 𝐓𝐀𝐙𝐄𝐄𝐁 𝐃𝐀𝐋𝐔 👳☪️"]  # typo alias used in rr.py / replyraid.py
 EMOJI = ["🔥", "💀", "⚡", "😈"]
 OneWord = ["!"]
 LOVE = ["❤️", "💕", "😘"]
